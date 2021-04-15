5eb8db47a573e6af8e7889fa482ba455539f2ab9
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-04-15 20:01:31 +0200

    Convert to tabs

--- startup.py
+++ startup.py
@@ -13,39 +13,39 @@ from dotenv import load_dotenv
 
 def get_env():
    # Récupérer les variables d'environnement (API Airtable)
-    env = join(dirname(__file__), '.env')
-    load_dotenv(env)
+	env = join(dirname(__file__), '.env')
+	load_dotenv(env)
 
 def get_startups_from_source():
-    # Récupérer des données CSV sur le site Beta Gouv
-    url = "https://beta.gouv.fr/api/v2.1/startups.json"
-    startups = json.loads(requests.get(url).text)
-    return startups.get('data')
+	# Récupérer des données CSV sur le site Beta Gouv
+	url = "https://beta.gouv.fr/api/v2.1/startups.json"
+	startups = json.loads(requests.get(url).text)
+	return startups.get('data')
 
 
 def find_new_se(beta_base, airtable_base):
-    # extraire des colonnes de la base
-    new_startups = []
+	# extraire des colonnes de la base
+	new_startups = []
 
-    # Liste d'ids dans Airtable
-    ids = [se['fields'].get('ID') for se in airtable_base]
+	# Liste d'ids dans Airtable
+	ids = [se['fields'].get('ID') for se in airtable_base]
 
-    for startup in beta_base:
+	for startup in beta_base:
 
-        if (startup.get('id') not in ids):
-            new_startups.append(startup)
+		if (startup.get('id') not in ids):
+			new_startups.append(startup)
 
-    return new_startups
+	return new_startups
 
 def print_new_se(new_se):
 	print("\nNouvelles Startups :")
 	for se in new_se:
-	    print("* {name} ({id}) - {phase} - {mission}".format(
-	    	name=se.get('attributes').get('name'), 
-	    	id=se.get('id'),
-	    	phase=se.get('attributes').get('phases')[-1].get('name'), 
-    		mission=se.get('attributes').get('pitch')
-	    ))
+		print("* {name} ({id}) - {phase} - {mission}".format(
+			name=se.get('attributes').get('name'), 
+			id=se.get('id'),
+			phase=se.get('attributes').get('phases')[-1].get('name'), 
+			mission=se.get('attributes').get('pitch')
+		))
 
 def load_new_se(new_se):
 	new_se = find_new_se(startups_source, startups_airtable)
