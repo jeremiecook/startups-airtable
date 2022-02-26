ae78df97076557eafc8efaebe47c5830cc616f69
Jeremie Cook <hello@jeremiecook.com>
2022-02-26 12:33:59 +0100

    Gestion de warnings

--- utils/Log.py
+++ utils/Log.py
@@ -4,10 +4,17 @@ from utils.Singleton import Singleton
 class Log(Singleton):
 
     logs = []
+    warnings = []
 
     def info(self, text):
         self.logs.append(text)
 
+    def warning(self, text):
+        self.warnings.append('- 🔸 ' + text)
+
     def print(self):
         for text in self.logs:
+            print(text)
+
+        for text in self.warnings:
             print(text)
